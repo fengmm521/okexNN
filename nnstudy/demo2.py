bba5a4179e73c8b71aa0c86e3c0db1ce0a38d242
woodcol <mage@woodcol.com>
2018-01-29 04:15:10 +0800

    demo2

--- nnstudy/demo2.py
+++ nnstudy/demo2.py
@@ -9,8 +9,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Import MNIST data
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=False)
+# from tensorflow.examples.tutorials.mnist import input_data
+# mnist = input_data.read_data_sets("/tmp/data/", one_hot=False)
 
 '''
 # Visualize decoder setting
@@ -70,69 +70,93 @@ def decoder(x):
 # Visualize encoder setting
 # Parameters
 learning_rate = 0.01    # 0.01 this learning rate will be better! Tested
-training_epochs = 100
+training_epochs = 50000
 batch_size = 256
-display_step = 1
+display_step = 100
 
 # Network Parameters
-n_input = 784  # MNIST data input (img shape: 28*28)
+n_input = 50  # MNIST data input (img shape: 28*28)
 
 # tf Graph input (only pictures)
 X = tf.placeholder("float", [None, n_input])
 
 # hidden layer settings
-n_hidden_1 = 128
-n_hidden_2 = 64
-n_hidden_3 = 10
-n_hidden_4 = 2
+#"klineS1":{"klineData":10,"layerEncode":[120,60,32,8],"mLayerNode":2,"layerDecode":[8,32,60,120],"state":"5minute","baseLine":1},
+n_hidden_1 = 120
+n_hidden_2 = 60
+n_hidden_3 = 32
+n_hidden_4 = 8
+n_hidden_5 = 2
 
 weights = {
     'encoder_h1': tf.Variable(tf.truncated_normal([n_input, n_hidden_1],)),
     'encoder_h2': tf.Variable(tf.truncated_normal([n_hidden_1, n_hidden_2],)),
     'encoder_h3': tf.Variable(tf.truncated_normal([n_hidden_2, n_hidden_3],)),
     'encoder_h4': tf.Variable(tf.truncated_normal([n_hidden_3, n_hidden_4],)),
+    'encoder_h5': tf.Variable(tf.truncated_normal([n_hidden_4, n_hidden_5],)),
 
-    'decoder_h1': tf.Variable(tf.truncated_normal([n_hidden_4, n_hidden_3],)),
-    'decoder_h2': tf.Variable(tf.truncated_normal([n_hidden_3, n_hidden_2],)),
-    'decoder_h3': tf.Variable(tf.truncated_normal([n_hidden_2, n_hidden_1],)),
-    'decoder_h4': tf.Variable(tf.truncated_normal([n_hidden_1, n_input],)),
+    'decoder_h1': tf.Variable(tf.truncated_normal([n_hidden_5, n_hidden_4],)),
+    'decoder_h2': tf.Variable(tf.truncated_normal([n_hidden_4, n_hidden_3],)),
+    'decoder_h3': tf.Variable(tf.truncated_normal([n_hidden_3, n_hidden_2],)),
+    'decoder_h4': tf.Variable(tf.truncated_normal([n_hidden_2, n_hidden_1],)),
+    'decoder_h5': tf.Variable(tf.truncated_normal([n_hidden_1, n_input],)),
 }
 biases = {
     'encoder_b1': tf.Variable(tf.random_normal([n_hidden_1])),
     'encoder_b2': tf.Variable(tf.random_normal([n_hidden_2])),
     'encoder_b3': tf.Variable(tf.random_normal([n_hidden_3])),
     'encoder_b4': tf.Variable(tf.random_normal([n_hidden_4])),
+    'encoder_b5': tf.Variable(tf.random_normal([n_hidden_5])),
 
-    'decoder_b1': tf.Variable(tf.random_normal([n_hidden_3])),
-    'decoder_b2': tf.Variable(tf.random_normal([n_hidden_2])),
-    'decoder_b3': tf.Variable(tf.random_normal([n_hidden_1])),
-    'decoder_b4': tf.Variable(tf.random_normal([n_input])),
+    'decoder_b1': tf.Variable(tf.random_normal([n_hidden_4])),
+    'decoder_b2': tf.Variable(tf.random_normal([n_hidden_3])),
+    'decoder_b3': tf.Variable(tf.random_normal([n_hidden_2])),
+    'decoder_b4': tf.Variable(tf.random_normal([n_hidden_1])),
+    'decoder_b5': tf.Variable(tf.random_normal([n_input])),
 }
 
 
 def encoder(x):
-    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']),
+    layer_1 = tf.nn.tanh(tf.add(tf.matmul(x, weights['encoder_h1']),
                                    biases['encoder_b1']))
-    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']),
+    layer_2 = tf.nn.tanh(tf.add(tf.matmul(layer_1, weights['encoder_h2']),
                                    biases['encoder_b2']))
-    layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, weights['encoder_h3']),
+    layer_3 = tf.nn.tanh(tf.add(tf.matmul(layer_2, weights['encoder_h3']),
                                    biases['encoder_b3']))
-    layer_4 = tf.add(tf.matmul(layer_3, weights['encoder_h4']),
-                                    biases['encoder_b4'])
-    return layer_4
+    layer_4 = tf.nn.tanh(tf.add(tf.matmul(layer_3, weights['encoder_h4']),
+                                    biases['encoder_b4']))
+    layer_5 = tf.add(tf.matmul(layer_3, weights['encoder_h5']),
+                                    biases['encoder_b5'])
+    return layer_5
 
 
 def decoder(x):
-    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']),
+    layer_1 = tf.nn.tanh(tf.add(tf.matmul(x, weights['decoder_h1']),
                                    biases['decoder_b1']))
-    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']),
+    layer_2 = tf.nn.tanh(tf.add(tf.matmul(layer_1, weights['decoder_h2']),
                                    biases['decoder_b2']))
-    layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, weights['decoder_h3']),
+    layer_3 = tf.nn.tanh(tf.add(tf.matmul(layer_2, weights['decoder_h3']),
                                 biases['decoder_b3']))
-    layer_4 = tf.nn.sigmoid(tf.add(tf.matmul(layer_3, weights['decoder_h4']),
+    layer_4 = tf.nn.tanh(tf.add(tf.matmul(layer_3, weights['decoder_h4']),
                                 biases['decoder_b4']))
-    return layer_4
+    layer_5 = tf.nn.tanh(tf.add(tf.matmul(layer_3, weights['decoder_h5']),
+                                biases['decoder_b5']))
+    return layer_5
 
+datapth = '../data/nndata/data5m_10.txt'
+
+
+def initData(pth = datapth):
+    f = open(pth,'r')
+    jstr = f.read()
+    f.close()
+    dats = json.loads(jstr)
+    # self.inputDatas = dats
+    # self.outDatas = self.inputDatas
+    print len(dats)
+    return dats
+
+X = initData()
 
 # Construct model
 encoder_op = encoder(X)
@@ -146,7 +170,11 @@ y_true = X
 # Define loss and optimizer, minimize the squared error
 cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
+dataset_size = len(X)
+
+training_epochs = 5000
 
 # Launch the graph
 with tf.Session() as sess:
@@ -157,18 +185,30 @@ with tf.Session() as sess:
     else:
         init = tf.global_variables_initializer()
     sess.run(init)
-    total_batch = int(mnist.train.num_examples/batch_size)
     # Training cycle
-    for epoch in range(training_epochs):
-        # Loop over all batches
-        for i in range(total_batch):
-            batch_xs, batch_ys = mnist.train.next_batch(batch_size)  # max(x) = 1, min(x) = 0
-            # Run optimization op (backprop) and cost op (to get loss value)
-            _, c = sess.run([optimizer, cost], feed_dict={X: batch_xs})
-        # Display logs per epoch step
-        if epoch % display_step == 0:
-            print("Epoch:", '%04d' % (epoch+1),
+
+    for i in range(training_epochs):
+        start = (i * batch_size) % dataset_size
+        end = min(start + batch_size,dataset_size)
+
+        batch_xs = X[start:end]
+
+        _, c = sess.run([optimizer, cost], feed_dict={X: batch_xs})
+
+        if i % display_step == 0:
+            print("Epoch:", '%08d' % (i+1),
                   "cost=", "{:.9f}".format(c))
+
+    # for epoch in range(training_epochs):
+    #     # Loop over all batches
+    #     for i in range(total_batch):
+    #         batch_xs, batch_ys = mnist.train.next_batch(batch_size)  # max(x) = 1, min(x) = 0
+    #         # Run optimization op (backprop) and cost op (to get loss value)
+    #         _, c = sess.run([optimizer, cost], feed_dict={X: batch_xs})
+    #     # Display logs per epoch step
+    #     if epoch % display_step == 0:
+    #         print("Epoch:", '%04d' % (epoch+1),
+    #               "cost=", "{:.9f}".format(c))
 
     print("Optimization Finished!")
 
@@ -182,8 +222,8 @@ with tf.Session() as sess:
     #     a[1][i].imshow(np.reshape(encode_decode[i], (28, 28)))
     # plt.show()
 
-    encoder_result = sess.run(encoder_op, feed_dict={X: mnist.test.images})
-    plt.scatter(encoder_result[:, 0], encoder_result[:, 1], c=mnist.test.labels)
-    plt.colorbar()
-    plt.show()
+    # encoder_result = sess.run(encoder_op, feed_dict={X: mnist.test.images})
+    # plt.scatter(encoder_result[:, 0], encoder_result[:, 1], c=mnist.test.labels)
+    # plt.colorbar()
+    # plt.show()
 
